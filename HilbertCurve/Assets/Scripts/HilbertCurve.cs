7e108e598dd6e796d8fcb68cfc60b5cc79c5816b
cjuillard <colin.juillard@gmail.com>
2022-05-01 21:36:18 -0500

    Refactor Hilbert curve logic into utility class

---
+++ HilbertCurve/Assets/Scripts/HilbertCurve.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using UnityEngine;
+
+namespace Runamuck
+{
+    [Serializable]
+    public class HilbertCurve
+    {
+        [Range(1, 8)]
+        [SerializeField] private int numberOfSplits = 1;
+        public int SquareSize { get; private set; } = 2;
+        public int NumberOfPoints { get; private set; } = 4;
+
+        public HilbertCurve(int numberOfSplits = 1)
+        {
+            this.numberOfSplits = numberOfSplits;
+        }
+
+        public void UpdateParams()
+        {
+            SquareSize = (int)Mathf.Pow(2, numberOfSplits);
+            NumberOfPoints = SquareSize * SquareSize;
+        }
+
+        public int NumberOfSplits
+        {
+            get => numberOfSplits;
+            set
+            {
+                numberOfSplits = Math.Max(1, value);
+            }
+        }
+
+        public Vector2 GetPosition(float t)
+        {
+            int i = Mathf.RoundToInt(t * NumberOfPoints);
+            // TODO for partials, find interpolated value between them
+            d2xy(NumberOfPoints, i, out int x1, out int y1);
+
+            return new Vector2(x1 / (float)SquareSize, y1 / (float)SquareSize);
+        }
+
+        ////convert (x,y) to d
+        //int xy2d(int n, int x, int y)
+        //{
+        //    int rx, ry, s, d = 0;
+        //    for (s = n / 2; s > 0; s /= 2)
+        //    {
+        //        rx = (x & s) > 0;
+        //        ry = (y & s) > 0;
+        //        d += s * s * ((3 * rx) ^ ry);
+        //        rot(n, &x, &y, rx, ry);
+        //    }
+        //    return d;
+        //}
+
+        // Code ported from C version on Wikipedia - https://en.wikipedia.org/wiki/Hilbert_curve
+        //It assumes a square divided into n by n cells, for n a power of 2, with integer coordinates, with (0,0) in the lower left corner,
+        // (n − 1, n − 1) in the upper right corner, and a distance d that starts at 0 in the lower left corner and goes to n^2 - 1 in the lower-right corner. 
+
+        //convert d to (x,y)
+        void d2xy(int n, int d, out int x, out int y)
+        {
+            int rx, ry, s, t = d;
+            x = y = 0;
+            for (s = 1; s < n; s *= 2)
+            {
+                rx = 1 & (t / 2);
+                ry = 1 & (t ^ rx);
+                rot(s, ref x, ref y, rx, ry);
+                x += s * rx;
+                y += s * ry;
+                t /= 4;
+            }
+        }
+
+        //rotate/flip a quadrant appropriately
+        void rot(int n, ref int x, ref int y, int rx, int ry)
+        {
+            if (ry == 0)
+            {
+                if (rx == 1)
+                {
+                    x = n - 1 - x;
+                    y = n - 1 - y;
+                }
+
+                //Swap x and y
+                int t = x;
+                x = y;
+                y = t;
+            }
+        }
+    }
+
+}
---
+++ HilbertCurve/Assets/Scripts/HilbertCurveAnimation.cs
@@ -0,0 +1,39 @@
+﻿using Runamuck;
+using UnityEngine;
+
+public class HilbertCurveAnimation : MonoBehaviour
+{
+    public Vector2 size = new Vector2(10, 10);
+    public HilbertCurve curve = new HilbertCurve();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+
+    private void OnValidate()
+    {
+        curve.UpdateParams();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+
+        Vector2 scale = new Vector2(size.x / (float)curve.SquareSize, size.y / (float)curve.SquareSize);
+        for (int i = 0, n = curve.NumberOfPoints; i < n - 1; i++)
+        {
+            Vector2 p1 = curve.GetPosition(i / (float)n);
+            Vector2 p2 = curve.GetPosition((i + 1) / (float)n);
+            
+            Gizmos.DrawLine(new Vector3(p1.x * size.x, p1.y * size.y, 0), new Vector3(p2.x * size.x, p2.y * size.y, 0));
+        }
+        Gizmos.DrawSphere(transform.position, .1f);
+    }
+}
